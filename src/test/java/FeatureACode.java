d19beb27b197f7874668478a68988ab331326277
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-02-09 18:05:26 +0100

    more work on feature a

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -1,5 +1,7 @@
 public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("Avesome work for A started");
+
+        System.out.println("all is good");
     }
 }
