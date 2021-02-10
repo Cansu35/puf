878143a3d711ee21b6173ac654fabea6904198f7
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-02-10 09:54:35 +0100

    movie1 branch final work is done,ready to merge not knowing spouse moved location of movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("future sdet insallah amin");
+        System.out.println("future sdet insallah amin ins");
         //watching more movie in the bedroom
+        System.out.println("more actions here");
+        System.out.println("ready to go back to living rooom");
     }
 }
