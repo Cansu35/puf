ca795b8edb67bf05946a2e518f1ee8998a8079a7
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-02-10 09:41:54 +0100

    movie1 updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("future sdet insallah");
+        System.out.println("future sdet insallah amin");
     }
 }
